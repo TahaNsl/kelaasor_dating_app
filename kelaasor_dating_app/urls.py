9e52cc453980483c1ddce0053ab1bf56559f8f64
TaHa <tahanasrollahii@gmail.com>
2025-10-06 13:38:48 +0330

    remove duplicate urls in users

--- kelaasor_dating_app/urls.py
+++ kelaasor_dating_app/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/auth/", include("users.urls")),
     path("api/match/", include("matching.urls")),
     path("api/users/", include("users.urls")),
 ]
